3c7b6e816f0bb338bbbd10cb3e1fb35a5ecfdb84
Johannes Donath <johannesd@torchmind.com>
2015-07-07 11:36:54 +0200

    Added a base interface for array property nodes

---
+++ api/src/main/java/com/torchmind/candle/api/IArrayPropertyNode.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2015 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.torchmind.candle.api;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+
+/**
+ * Represents a property assignment node that uses array values.
+ * @author Johannes Donath
+ */
+public interface IArrayPropertyNode extends IPropertyNode {
+
+        /**
+         * Retrieves the array item type.
+         * @return The type.
+         */
+        @Nonnull
+        NodeValueType itemType ();
+
+        /**
+         * Retrieves the array length.
+         * @return The length.
+         */
+        @Nonnegative
+        int length ();
+
+        /**
+         * {@inheritDoc}
+         */
+        @Nonnull
+        @Override
+        default NodeValueType valueType () {
+                return NodeValueType.ARRAY;
+        }
+}
