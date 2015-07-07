160fcfe896114e933d101c8ca600a8715a695335
Johannes Donath <johannesd@torchmind.com>
2015-07-07 09:53:33 +0200

    Added base node definition

---
+++ api/src/main/java/com/torchmind/candle/api/NodeType.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2015 Johannes Donath <johannesd@torchmind.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.torchmind.candle.api;
+
+/**
+ * @author Johannes Donath
+ */
+public enum NodeType {
+        /**
+         * Represents a Single-Line or Multi-Line comment.
+         * Nodes that identify themselves as comment nodes implement {@link com.torchmind.candle.api.ICommentNode}.
+         */
+        COMMENT,
+
+        /**
+         * Represents an inclusion.
+         */
+        INCLUDE,
+
+        /**
+         * Represents an object.
+         */
+        OBJECT,
+
+        /**
+         * Represents a property.
+         */
+        PROPERTY
+}
